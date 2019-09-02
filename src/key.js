fba0d2b79b93e5e3664e47169ebe2cd29482ac90
terminatorDX <jain.cj.chetan@gmail.com>
2019-09-03 01:05:03 +0530

    changes in key which exports 2 var in stead of one

--- src/key.js
+++ src/key.js
@@ -1,4 +1,2 @@
-export const data = {
-    ApiKey: "4b54b9f509eedba0aa7a04bd0c0fd507",
-    CorsDisable: "http://cors-anywhere.herokuapp.com"
-};
+export const ApiKey = "4b54b9f509eedba0aa7a04bd0c0fd507";
+export const CorsDisable = "http://cors-anywhere.herokuapp.com";
